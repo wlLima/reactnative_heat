440e22278851e0d8fdf784fe81f12876a25c598d
wlLima <lima14.wl@gmail.com>
2021-10-20 22:07:40 -0300

    💄 change assets

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import { styles } from "./style";
+export function Home() {
+
+    return (
+        <View style={styles.container}>
+            <Text>Hello World! </Text>
+        </View>
+    )
+
+}
+
+
